4f2a0f8d9f0ce96a75b500988f316aa750e22508
noy00y <69055108+noy00y@users.noreply.github.com>
2021-07-28 19:00:30 -0400

    First Push to Github
    Full stack application using the MERN stack for photo uploading

---
+++ server/routes/posts.js
@@ -0,0 +1,13 @@
+import express from 'express'
+
+import {getPosts, createPost} from '../controllers/posts.js'
+
+
+const router = express.Router()
+
+router.get('/', getPosts)
+router.get('/', createPost)
+
+export default router
+
+// http://localhost:5000/posts
