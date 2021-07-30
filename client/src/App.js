82a329db634a2ac358b561cdae76f05bbcac47e2
noy00y <69055108+noy00y@users.noreply.github.com>
2021-07-29 22:06:32 -0400

    Adding JSX

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core'
 import Posts from './components/Posts/Posts'
 import Form from './components/Form/Form'
 import useStyles from './styles'
 import memories from './images/memories.png'
+import {useDispatch} from 'react-redux'
+import {getPosts} from './actions/posts'
+
 const App = () => {
     const classes = useStyles()
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(getPosts)
+    }, [])
+
+
     return(
         <Container maxidth="lg">
             <AppBar className = {classes.appBar} position = "static" color = "inherit">
