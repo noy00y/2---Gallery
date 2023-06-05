387fb1fc8a9a74cb90fc1b93b34a7fa3b97023db
Ozair Khan <69055108+noy00y@users.noreply.github.com>
2023-06-05 08:20:51 -0400

    removing database credentials

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://noy00y:330614678@cluster0.quhfg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://noy00y:@cluster0.quhfg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
